818190cd35e4645cf1ecebf7a9509524d87cb63c
Andrew Zurodov <zurodov@gmail.com>
2015-03-18 12:19:09 +0200

    modified:   makesymlinks.sh 	corrected .zshrc symlink bug

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 ############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# .makesymlinks.sh
+# creates symlinks to any desired dotfiles in ~/dotfiles
 ############################
-
-########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles/old             # old dotfiles backup directory
 files="alias vimrc zshrc Xresources"    # list of files/folders to symlink in homedir
 
-##########
-
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles/old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
 
 function install_zsh {
 # Test to see if zshell is installed.  If it is:
@@ -52,4 +35,15 @@ install_zsh
 
 # add custom oh_my_zsh config
 ln -s $dir/00-zur.zsh ~/.oh-my-zsh/custom/00-zur.zsh
+# Vundle install
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+cd $dir
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles/old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
 
